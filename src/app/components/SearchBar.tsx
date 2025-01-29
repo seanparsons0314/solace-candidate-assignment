c4beb0aee9c1fd474dfffe4c13a1b84b826e0af5
Sean Parsons <seanparsons314@gmail.com>
2025-01-28 19:16:12 -0600

    feat: update search bar to support enter key event

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 
 interface SearchBarProps {
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearch: (keyword: string) => void;
   onReset: () => void;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ onChange, onReset }) => {
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch, onReset }) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onSearch(e.target.value)
+  }
+
+  const onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSearch((e as unknown as React.ChangeEvent<HTMLInputElement>).target.value);
+    }
+  }
+
   return (
     <div className="flex flex-col sm:flex-row mb-6">
       <input
         className="flex-grow border border-gray-400 p-2 rounded-l sm:mr-2 mb-2 sm:mb-0"
         onChange={onChange}
+        onKeyPress={onKeyPress}
       />
       <button
         className="bg-blue-500 text-white p-2 rounded-r"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,17 +18,15 @@ export default function Home() {
     });
   }, []);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = e.target.value;
-    document.getElementById("search-term")!.innerHTML = searchTerm;
+  const onSearch = (keyword: string) => {
     console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => (
-      advocate.firstName.includes(searchTerm) ||
-      advocate.lastName.includes(searchTerm) ||
-      advocate.city.includes(searchTerm) ||
-      advocate.degree.includes(searchTerm) ||
-      advocate.specialties.includes(searchTerm) ||
-      advocate.phoneNumber.toString().includes(searchTerm)
+      advocate.firstName.includes(keyword) ||
+      advocate.lastName.includes(keyword) ||
+      advocate.city.includes(keyword) ||
+      advocate.degree.includes(keyword) ||
+      advocate.specialties.includes(keyword) ||
+      advocate.phoneNumber.toString().includes(keyword)
     ));
     setFilteredAdvocates(filteredAdvocates);
   };
@@ -37,10 +35,11 @@ export default function Home() {
     setFilteredAdvocates(advocates);
   };
 
+
   return (
     <main className="p-6 max-w-7xl mx-auto">
       <h1 className="text-3xl font-bold mb-6 text-center">Solace Advocates</h1>
-      <SearchBar onChange={onChange} onReset={onResetSearch} />
+      <SearchBar onSearch={onSearch} onReset={onResetSearch} />
       <AdvocateTable advocates={filteredAdvocates} />
     </main>
   );
